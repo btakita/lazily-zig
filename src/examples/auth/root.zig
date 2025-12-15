6be4ca3676c857d4e60b5ab753119d492eb9a90e
Brian Takita <brian.takita@gmail.com>
2025-12-15 04:31:02 -0500

    Fix Cell implementation. Add tests. Major refactoring

--- src/examples/auth/root.zig
+++ src/examples/auth/root.zig
@@ -1,77 +1,68 @@
 const std = @import("std");
 const lazily = @import("lazily");
+const Context = lazily.Context;
+const slot = lazily.slot;
+const deinitValue = lazily.deinitValue;
+const Owned = lazily.Owned;
+const OwnedString = lazily.OwnedString;
+const StringView = lazily.StringView;
 
-const AuthToken = []const u8;
+const AuthToken = OwnedString;
 
-fn authenticate() AuthToken {
+fn authenticate() []const u8 {
     std.debug.print("Authenticating...\n", .{});
     return "very_secret_token";
 }
 
-const deinitAuthToken = lazily.deinitValue(AuthToken);
+const deinitAuthToken = deinitValue(AuthToken, null);
 
-fn getAuthToken(ctx: *lazily.Context) !AuthToken {
+fn getAuthToken(ctx: *Context) !AuthToken {
     const auth_token = authenticate();
-    return try ctx.allocator.dupe(u8, auth_token);
+    return AuthToken.managed(try ctx.allocator.dupe(u8, auth_token));
 }
 
 // Lazily get an Auth Token using the lazily.slot function.
 // Which accepts separate value getter function and optional deinit functions.
-pub fn slotAuthToken(ctx: *lazily.Context) !AuthToken {
-    return try lazily.slot(AuthToken, ctx, getAuthToken, deinitAuthToken);
+pub fn slotAuthToken(ctx: *Context) !AuthToken {
+    return try slot(AuthToken, ctx, getAuthToken, deinitAuthToken);
 }
 test "slotAuthToken" {
-    const ctx = try lazily.Context.init(std.testing.allocator);
+    const ctx = try Context.init(std.testing.allocator);
     defer ctx.deinit();
     const token = try slotAuthToken(ctx);
-    try std.testing.expectEqualStrings("very_secret_token", token);
-    try std.testing.expectEqualStrings("very_secret_token", try slotAuthToken(ctx));
+    try std.testing.expectEqualStrings("very_secret_token", token.value);
+    try std.testing.expectEqualStrings(
+        "very_secret_token",
+        (try slotAuthToken(ctx)).value,
+    );
 }
 
-fn getAuthTokenWithDeinit(ctx: *lazily.Context) !lazily.WithDeinit(AuthToken) {
-    const auth_token = authenticate();
-    return .{
-        .value = try ctx.allocator.dupe(u8, auth_token),
-        .deinit = deinitAuthToken,
-    };
-}
-
-// Lazily get an Auth Token using the lazily.slotWithDeinit function.
-// Which accepts a getter function for a lazily.WithDeinit(T) struct that holds the value and optional deinit functions.
-pub fn slotAuthTokenWithDeinit(ctx: *lazily.Context) !AuthToken {
-    return try lazily.slotWithDeinit(AuthToken, ctx, getAuthTokenWithDeinit);
-}
-test "slotAuthTokenWithDeinit" {
-    const ctx = try lazily.Context.init(std.testing.allocator);
-    defer ctx.deinit();
-    const token = try slotAuthTokenWithDeinit(ctx);
-    try std.testing.expectEqualStrings("very_secret_token", token);
-    try std.testing.expectEqualStrings("very_secret_token", try slotAuthTokenWithDeinit(ctx));
-}
-
-pub const slotAuthToken_slotFn = lazily.slotFn(
+pub const slotAuthToken_initSlotFn = lazily.initSlotFn(
     AuthToken,
     getAuthToken,
     deinitAuthToken,
 );
-test "slotFn (slotAuthToken_slotFn)" {
-    const ctx = try lazily.Context.init(std.testing.allocator);
+test "initSlotFn (slotAuthToken_initSlotFn)" {
+    const ctx = try Context.init(std.testing.allocator);
     defer ctx.deinit();
-    const token = try slotAuthToken_slotFn(ctx);
-    try std.testing.expectEqualStrings("very_secret_token", token);
-    try std.testing.expectEqualStrings("very_secret_token", try slotAuthToken_slotFn(ctx));
+    const token = try slotAuthToken_initSlotFn(ctx);
+    try std.testing.expectEqualStrings("very_secret_token", token.value);
+    try std.testing.expectEqualStrings(
+        "very_secret_token",
+        (try slotAuthToken_initSlotFn(ctx)).value,
+    );
 }
 
-export fn slotAuthTokenFFI(ctx: *lazily.Context) callconv(.c) lazily.StringView {
-    const token = slotAuthTokenWithDeinit(ctx) catch |err| {
-        return lazily.StringView{
+export fn slotAuthTokenFFI(ctx: *Context) callconv(.c) StringView {
+    const token = slotAuthToken(ctx) catch |err| {
+        return StringView{
             .ptr = &.{},
             .len = 0,
             .errno = 1,
             .errmsg = @errorName(err).ptr,
         };
     };
-    return lazily.StringView.fromSlice(token);
+    return StringView.fromSlice(token.value);
 }
 comptime {
     // Support both camelCase and snake_case for FFI functions that target platforms with different name conventions.
--- src/root.zig
+++ src/root.zig
@@ -2,20 +2,22 @@
 const std = @import("std");
 const cell_mod = @import("./lazily/cell.zig");
 pub const cell = cell_mod.cell;
+pub const cellFn = cell_mod.cellFn;
+pub const CellFn = cell_mod.CellFn;
 pub const Cell = cell_mod.Cell;
 const ctx_mod = @import("./lazily/context.zig");
 pub const Context = ctx_mod.Context;
+pub const Owned = ctx_mod.Owned;
+pub const Slot = ctx_mod.Slot;
+pub const OwnedString = ctx_mod.OwnedString;
+pub const valueFnCacheKey = ctx_mod.valueFnCacheKey;
+pub const ValueFn = ctx_mod.ValueFn;
 pub const Graph = @import("./lazily/graph.zig").Graph;
 const slot_mod = @import("./lazily/slot.zig");
-pub const Compute = slot_mod.WithDeinitFn;
 pub const deinitValue = slot_mod.deinitValue;
 pub const slot = slot_mod.slot;
-pub const slotWithDeinit = slot_mod.slotWithDeinit;
-pub const slotFn = slot_mod.slotFn;
-pub const SlotFn = slot_mod.SlotFn;
+pub const initSlotFn = slot_mod.initSlotFn;
 pub const StringView = slot_mod.StringView;
-pub const WithDeinit = slot_mod.WithDeinit;
-pub const WithDeinitFn = slot_mod.WithDeinitFn;
 
 test {
     std.testing.refAllDecls(@This());
